ef56ee1826e8e2c9faf4ddc779624ad7612373ec
ishandhanani <ishandhanani@gmail.com>
2025-05-25 09:51:47 -0700

    style(layout): remove Darkmode component from header

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -4,7 +4,7 @@ import * as Component from "./quartz/components"
 // components shared across all pages
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
-  header: [Component.PageTitle(), Component.Spacer(), Component.Darkmode()],
+  header: [Component.PageTitle(), Component.Spacer()],
   afterBody: [],
   footer: Component.Footer({
     links: {
