28808465c2115af4c12bba23f4472768f2233a1c
ishandhanani <ishandhanani@gmail.com>
2025-05-24 19:03:00 -0700

    refactor(layout): remove PageTitle component from list page layout

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -50,7 +50,6 @@ export const defaultContentPageLayout: PageLayout = {
 export const defaultListPageLayout: PageLayout = {
   beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta()],
   left: [
-    Component.PageTitle(),
     Component.MobileOnly(Component.Spacer()),
     Component.Flex({
       components: [
